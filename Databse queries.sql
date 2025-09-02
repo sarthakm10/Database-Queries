7e5f1f35c9bc9829586f0466e821c40a70b6e71a
sarthakm10 <mishrasarthak33@gmail.com>
2025-09-03 00:00:51 +0530

    added db queries

---
+++ Databse queries.sql
@@ -0,0 +1,11 @@
+create database test;
+	-- to create database
+show databases;
+	-- to show all the database
+use test;
+	-- to use the created database
+select database();
+	-- to show selected databse
+drop database test;
+	-- to delete database
+
